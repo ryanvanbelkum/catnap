633220e31f3b2127f8ff1677f8a16c2265988553
ryanvanbelkum <hello@ryanvanbelkum.me>
2023-09-19 07:33:22 -0500

    Initial Commit

---
+++ Cat Nap/Cat_NapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_NapApp.swift
+//  Cat Nap
+//
+//  Created by Ryan Vanbelkum on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Cat_NapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat Nap/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cat Nap
+//
+//  Created by Ryan Vanbelkum on 9/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
